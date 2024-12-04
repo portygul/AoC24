57d494cd92873a63175acae65e7e91e7b364774c
Lu Schneider <luise.schneider@zaumstudio.com>
2024-12-04 21:09:01 +0100

    update day 4 ;o)

--- src/Dec4/day4.ts
+++ src/Dec4/day4.ts
@@ -2,21 +2,21 @@ import { match } from "assert";
 import { readLines } from "../util/helper";
 
 const sample = await readLines('Sample.txt');
+const directions = [{x: 0, y: 1}, {x: 0, y: -1},{x: 1, y: 0}, {x: -1, y: 0}, {x: 1, y: 1}, {x: 1, y: -1}, {x: -1, y: 1}, {x: -1, y: -1}];
+
+console.log("XMAS count:  " + findAllXmasses());
+console.log("X-MAS count: " + findAllCrossmasses())
 
 function findAllXmasses()
 {
     let XmasCount = 0;
     for(let row = 0; row < sample.length; row++)
     {
-        XmasCount += countForwardXmasMatches(sample[row]); 
-        XmasCount += countBackwardMatches(sample[row]);
-        XmasCount += countVertXmasMatches(sample[row], row, 1);
-        XmasCount += countVertXmasMatches(sample[row], row, -1);
-        XmasCount += countDiagXmasMatches(sample[row], row, 1, 1);
-        XmasCount += countDiagXmasMatches(sample[row], row, 1, -1);
-        XmasCount += countDiagXmasMatches(sample[row], row, -1, 1);
-        XmasCount += countDiagXmasMatches(sample[row], row, -1, -1);
-    };
+        directions.forEach(dir => 
+        {
+        XmasCount += countDirectionalXmasMatches(sample[row], row, dir.x, dir.y); 
+        });
+    }
     return XmasCount;
 }
 
@@ -30,14 +30,14 @@ function findAllCrossmasses()
     return crossCount;
 }
 
-console.log("XMAS count:  " + findAllXmasses());
-console.log("X-MAS count: " + findAllCrossmasses())
-
-
 function countCrossmassMatches(line, row)
 {
     const matchIndices = [...line.matchAll(/A/g)].map((elem) => elem.index);
+    
     let matchCount = 0;
+    const charCodeS = 'S'.charCodeAt(0);
+    const charCodeM = 'M'.charCodeAt(0);
+    const charCodeSPlusM = charCodeM + charCodeS;
 
     if(matchIndices.length === 0)
     {
@@ -46,83 +46,25 @@ function countCrossmassMatches(line, row)
 
     for(let i = 0; i < matchIndices.length; i++)
     {
-        let diaCount = 0;
         const index = matchIndices[i];
 
-        if(sample[row-1]?.charAt(index-1) === 'S')
-        {
-            diaCount += sample[row+1]?.charAt(index+1) === 'M' ? 1 : 0;
-        }
-        else if(sample[row-1]?.charAt(index-1) === 'M')
-        {
-            diaCount += sample[row+1]?.charAt(index+1) === 'S' ? 1 : 0;
-        }
-        if(sample[row-1]?.charAt(index+1) === 'M')
-        {
-            diaCount += sample[row+1]?.charAt(index-1) === 'S' ? 1 : 0;
-        }
-        else if(sample[row-1]?.charAt(index+1) === 'S')
-        {
-            diaCount += sample[row+1]?.charAt(index-1) === 'M' ? 1 : 0;
-        }
-        matchCount += diaCount === 2 ? 1 : 0;
-    }
-    return matchCount;
-}
+        const topLeft = sample[row-1]?.charCodeAt(index-1);
+        const topRight = sample[row-1]?.charCodeAt(index+1);
 
-function countForwardXmasMatches(line: string)
-{
-    return line.match(/XMAS/g)?.length ?? 0
-}
-
-function countBackwardMatches(line: string)
-{
-    return line.match(/SAMX/g)?.length ?? 0
-}
-
-/**
- * upOrDown: up: -1; down: +1
- */
-function countVertXmasMatches(line: string, row: number, upOrDown: number)
-{
-    const matchIndices = [...line.matchAll(/X/g)].map((elem) => elem.index)
-    let matchCount = 0;
-
-    if(matchIndices.length === 0)
-        {
-            return 0;
-        }
-    for(let i = 0; i < matchIndices.length; i++)
-    {
-        const matchIndex = matchIndices[i];
-        if(matchIndex === undefined)
+        if((topLeft !== charCodeS && topLeft !== charCodeM) || (charCodeSPlusM - topLeft - sample[row+1]?.charCodeAt(index+1)) !== 0)
         {
             continue;
         }
-        if(!sample[row+upOrDown]?.charAt(matchIndex) || sample[row+upOrDown].charAt(matchIndex) !== 'M')
-        {
-            continue;
-        }
-        if(!sample[row+(2*upOrDown)]?.charAt(matchIndex) || sample[row+(2*upOrDown)].charAt(matchIndex) !== 'A')
-        {
-            continue;
-        }
-        if(!sample[row+(3*upOrDown)]?.charAt(matchIndex) || sample[row+(3*upOrDown)].charAt(matchIndex) !== 'S')
+        if((topRight !== charCodeS && topRight !== charCodeM) || ((charCodeSPlusM - topRight - sample[row+1]?.charCodeAt(index-1)) !== 0))
         {
             continue;
         }
         matchCount++;
-    };
+    }
     return matchCount;
 }
 
-/**
- * upOrDown: up: -1; down: +1
- * leftOrRight: left: -1; right +1
- */
-function countDiagXmasMatches(line: string, row: number, upOrDown: number, leftOrRight: number) {
-
-    let count = 0;
+function countDirectionalXmasMatches(line: string, row: number, deltaX: number, deltaY: number,) {
 
     const matches = [...line.matchAll(/X/g)];
     const matchIndices = matches.map((elem) => elem.index)
@@ -139,15 +81,15 @@ function countDiagXmasMatches(line: string, row: number, upOrDown: number, leftO
         {
             continue;
         }
-        if(sample[row+upOrDown]?.charAt(matchIndex+leftOrRight) !== 'M')
+        if(sample[row+deltaY]?.charAt(matchIndex+deltaX) !== 'M')
         {
             continue;
         }
-        if(sample[row+(2*upOrDown)]?.charAt(matchIndex+(2*leftOrRight)) !== 'A')
+        if(sample[row+(2*deltaY)]?.charAt(matchIndex+(2*deltaX)) !== 'A')
         {
             continue;
         }
-        if(sample[row+(3*upOrDown)]?.charAt(matchIndex+(3*leftOrRight)) !== 'S')
+        if(sample[row+(3*deltaY)]?.charAt(matchIndex+(3*deltaX)) !== 'S')
         {
             continue;
         }
